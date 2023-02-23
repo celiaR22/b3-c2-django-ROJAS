1c815ccd89757b9e20699ad68208d9c288969547
Célia Rojas <celia.rojas@outlook.fr>
2023-02-23 10:00:41 +0100

    install Pillow + model Lessons + migrations + createsuperuser

--- src/booking/models.py
+++ src/booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class schoolLessons (models.Model):
+    schoolName = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, default= 'test')
+    image = models.ImageField(upload_to="image", blank=True, null =True)
+    nbPerson = models.IntegerField(default = 0)
+    price = models.FloatField(default = 0.0)
+    date =models.DateField(auto_now= True)
+    description = models.TextField(blank=True)
+
+def __str__(self):
+    return self.title
