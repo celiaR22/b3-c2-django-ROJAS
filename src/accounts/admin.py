863c3452c7f3149d7e53011fb68a83c7c13ad85c
Célia Rojas <celia.rojas@outlook.fr>
2023-03-13 11:21:07 +0100

    login user + navigation principale

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from accounts.models import customerUser
 
-# Register your models here.
+admin.site.register(customerUser)
--- src/djangoProject/urls.py
+++ src/djangoProject/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from booking.views import index, event_detail
 from django.conf.urls.static import static
 from django.conf import settings
-from accounts.views import signupForm
+from accounts.views import login_user, logout_user, signupForm
 
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls, name="admin"),
     path('event<int:id>/', event_detail, name="event_detail"),
-    path('signup', signupForm, name="signup")
+    path('signup/', signupForm, name="signup"),
+    path('logout/', logout_user, name="logout"),
+    path('login/', login_user, name="login")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
