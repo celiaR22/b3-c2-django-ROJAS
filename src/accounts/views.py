42cdb15a97a6aff6b9e09873114b32930f14a2f7
Célia Rojas <celia.rojas@outlook.fr>
2023-03-13 14:31:49 +0100

    account

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth import get_user_model, login, logout, authenticate
 
 User = get_user_model()
 
@@ -14,6 +14,17 @@ def signupForm(request):
     
     return render(request, 'accounts/signupForm.html')
 
-def logout(request):
-    logout(request)
-    return redirect('index')
+def logout_user(request):
+  logout(request)
+  return redirect('index')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user =authenticate(username=username,password= password)
+        if user:
+            login(request, user)
+            return redirect('index')
+        
+    return render(request, 'accounts/login.html')
