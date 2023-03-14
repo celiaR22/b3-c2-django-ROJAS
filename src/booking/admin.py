3e53bb2349702981f285ac33b75ef5c52f283b9b
Célia Rojas <celia.rojas@outlook.fr>
2023-03-14 17:08:50 +0100

    creation model reservations et cart

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from booking.models import pilotEvent
+from booking.models import Cart, Reservation, pilotEvent
 
 # Register your models here.
 admin.site.register(pilotEvent)
+admin.site.register(Reservation)
+admin.site.register(Cart)
--- src/booking/models.py
+++ src/booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from djangoProject.settings import AUTH_USER_MODEL
 
 class pilotEvent (models.Model):
     schoolName = models.CharField(max_length=128)
@@ -15,4 +16,24 @@ class pilotEvent (models.Model):
 
     def get_absolute_url(self):
         return reverse("event_detail", kwargs={"id": self.id})
+    
+class Reservation(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pilotEvent =models.ForeignKey(pilotEvent, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.pilotEvent.schoolName} ({self.quantity})"
+    
+class Cart(models.Model):
+    user= models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    reservation = models.ManyToManyField(Reservation)
+    ordered = models.BooleanField(default = False)
+    
+    def __str__(self):
+        return self.user
+
+    
+    
 
